4ded733d159b569e678cf61847c03d7f4209dabb
Aiden Cullo <aidencullo@gmail.com>
2023-07-05 19:19:25 -0300

    add exercises to numbers tutorial

--- python/datatypes/numbers.py
+++ python/datatypes/numbers.py
@@ -15,3 +15,19 @@ jordan = 1
 # this is a variables that has a value of "hello"
 
 greeting = "hello"
+
+# Let's do an exercise!
+
+## Exercise
+
+# say we have some apples and oranges. We have 10 apples and 5 oranges
+
+# TASK Create a variable apples and store 10 in it
+
+# TASK Create a variable oranges and store 5 in it
+
+# TASK Create a variable fruits and set it to 0
+
+# TASK Set fruits to the addition of oranges and fruits
+
+# CHECK check to see that fruits is 15, by printing it to the terminal.
