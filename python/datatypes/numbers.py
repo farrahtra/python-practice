233cd4f08da7d885302248ab8870f8696710d88b
Aiden Cullo <aidencullo@gmail.com>
2023-07-05 18:57:30 -0300

    number data tutorial

---
+++ python/datatypes/numbers.py
@@ -0,0 +1,17 @@
+# today we will be learning about numbers in python!
+
+# before we learn about number, we need to learn about variables
+
+# this is a variables that has a value of 0
+
+jean = 0
+
+# variables can be almost anything, here is another variable named 'jordan' with the value with the value 1
+
+jordan = 1
+
+# variables can also have string values (text)
+
+# this is a variables that has a value of "hello"
+
+greeting = "hello"
