11f68d12dd71b88b848330d7079e8a046d2bead1
farrahtra <trari.farrah@gmail.com>
2023-07-17 22:34:36 -0300

    finished function lesson

---
+++ python/functions/functions1.py
@@ -0,0 +1,62 @@
+# functions
+
+# say we want to print out a few things
+
+print("my name is aiden")
+print("i am 25 years old")
+print("i live in buenos aires")
+
+# now say for some reason we want to print the same thing again
+
+print("my name is aiden")
+print("i am 25 years old")
+print("i live in buenos aires")
+
+# now say we want to print it out a third time...
+
+# ok this is getting a big tiring, there must be a better way...
+
+# enter function...
+
+# we will create a function in python to execute this code
+
+def sayBio():    
+    print("my name is aiden")
+    print("i am 25 years old")
+    print("i live in buenos aires")
+
+# dont forget the indentation
+
+# and now we can just call this function
+
+print("printing bio twice")
+
+sayBio();
+sayBio();
+
+# exercise 1
+
+# write and call a function to print out the days of the week, one on each line
+
+def daysweek():  
+	print("monday")
+	print("tuesday")
+	print("wednesday") 
+	print("thursday")
+	print("friday")
+	print("saturday")
+	print("sunday")
+
+print ("printing daysweek twice")
+
+daysweek();
+daysweek();
+
+
+ 
+
+
+
+# exercise 2
+
+# write and call a function to print out your name and birthday
